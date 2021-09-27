e4e9484b5d7fd9ed6e007525fd63ef0ef5918e4a
Pawan Rama Mali <prm@outlook.in>
2021-09-27 22:11:36 +0530

    updated model build

--- training.py
+++ training.py
@@ -69,6 +69,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('ai_chatbot.model')
+ai_model = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('ai_chatbot_model.h5', ai_model)
 print("AI Chatbot Model Built")
